22169464e1d37bb5b6bdee5bb980cab4827510b8
aargarcia <aargarcia@aidacanarias.com>
2025-03-09 14:01:54 +0000

    refactor create new project with react

--- my-blog/src/app/components/profile-section.jsx
+++
@@ -1,8 +0,0 @@
-export function ProfileSection({userName,name}){
-    return (
-        <div>
-          <img className="avatar-img " src={`https://unavatar.io/${userName}`} alt="Avatar" />
-          <h2>{name}</h2>
-        </div>
-      );
-    };
--- my-blog/src/app/page.jsx
+++
@@ -1,9 +0,0 @@
-import './globals.css'
-import {Profile} from './components/profile'
-export default function Home() {
-  return (
-    <div>
-      <Profile/>
-    </div>
-  );
-}
