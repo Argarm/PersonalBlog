16b6100db09528376f74c011177704e94a0490d7
argarm <aarongrmrr@gmail.com>
2025-03-14 11:09:54 +0000

    refactor: rename navbar to capitalCamelCase middlestep

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import Navbar from './Navbar';
+import Navbar from './Navbar2';
 import styles from '../styles/components/layout.module.css';
 
 export default function Layout({ children }) {
--- components/Navbar2.js
+++ components/Navbar2.js
@@ -4,7 +4,6 @@ import { useLanguage } from '../context/LanguageContext';
 
 export default function Navbar() {
   const { t } = useLanguage();
-  
   return (
     <nav className={styles.navbar}>
       <div className={styles.navLinks}>
